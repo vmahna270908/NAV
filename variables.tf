5b0402ac3be05079c9e99f7fda5b73dcbb563fa2
vmahna270908 <vaibhavmahna@outlook.com>
2024-12-13 01:31:59 -0500

    Create vNet and Configure

--- variables.tf
+++ variables.tf
@@ -1,29 +1,11 @@
-variable "rg_name" {
+variable "NAV_vnet_Name" {
   type        = string
-  default     = "AVD-RG"
-  description = "Name of the Resource group in which to deploy service objects"
-}
-
-variable "AVD_vnet_Name" {
-  type        = string
-  default     = "AVD_vNet-1"
+  default     = "NAV_vNet"
   description = "vNet1 Name"
 }
 
-variable "AVD_vNet_Address_Space" {
+variable "NAV_vNet_Address_Space" {
   type        = list(string)
-  default     = ["10.0.1.0/25"]
+  default     = ["172.16.1.0/24"]
   description = "vNet1 Address Space"
-}
-
-variable "AVD_vnet_Name_2" {
-  type        = string
-  default     = "AVD_vNet-2"
-  description = "vNet2 Name"
-}
-
-variable "AVD_vNet_Address_Space_2" {
-  type        = list(string)
-  default     = ["10.0.1.128/25"]
-  description = "vNet2 Address Space"
 }
