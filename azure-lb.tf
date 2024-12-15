759e768c685dff1742b07b1f9d25d545255a153f
vmahna270908 <vaibhavmahna@outlook.com>
2024-12-15 00:43:43 -0500

    Adding the Web Server to the Backend Pool

--- azure-lb.tf
+++ azure-lb.tf
@@ -47,6 +47,14 @@ resource "azurerm_network_interface" "AZ-NAV-App-NIC" {
   }
 }
 
+# Associate Network Interface to the Backend Pool of the Load Balancer
+resource "azurerm_network_interface_backend_address_pool_association" "AZ-NAV-LB-Pool-Attach" {
+  count                   = 2
+  network_interface_id    = azurerm_network_interface.AZ-NAV-App-NIC[count.index].id
+  ip_configuration_name   = "ipconfig${count.index}"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.AZ-NAV-LB_Pool.id
+}
+
 # Create Virtual Machine
 resource "azurerm_linux_virtual_machine" "AZ-NAV-App-Servers" {
   count                 = 2
@@ -132,13 +140,5 @@ resource "azurerm_lb_outbound_rule" "AZ-NAV-LB_lboutbound_rule" {
   name                    = "NAV-LB-Outbound"
   loadbalancer_id         = azurerm_lb.AZ-NAV-LB.id
   protocol                = "Tcp"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.AZ-NAV-LB_Pool.id
-}
-
-# Associate Network Interface to the Backend Pool of the Load Balancer
-resource "azurerm_network_interface_backend_address_pool_association" "AZ-NAV-LB-Pool" {
-  count                   = 2
-  network_interface_id    = azurerm_network_interface.AZ-NAV-App-NIC[count.index].id
-  ip_configuration_name   = "ipconfig${count.index}"
   backend_address_pool_id = azurerm_lb_backend_address_pool.AZ-NAV-LB_Pool.id
 }
